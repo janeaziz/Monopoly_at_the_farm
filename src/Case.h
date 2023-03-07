d4caf077179e913dce7bb7d042d4f9bc890e4b53
AZIZ JANE <jane.aziz@etu.univ-lyon1.fr>
2023-03-07 05:49:53 -0500

    ajout fichier Case.h Couleur.h et Couleur.cpp

--- src/Case.h
+++ src/Case.h
@@ -1,7 +1,16 @@
 #ifndef _CASE_H
-#define _CASE_H
+#define  _CASE_H
+#include <string>
+#include"Couleur.h"
 
 class Case{
+    private:
+
+    int pos;
+    std::string type;
+    Couleur c;
+
+    
 
 };
 
